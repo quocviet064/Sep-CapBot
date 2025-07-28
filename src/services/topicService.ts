0bc6854032bccfcbb9f3da99cca11c7d00420feb
ToKhuyet <banghuu7@gmail.com>
2025-07-28 16:01:23 +0700

    revert commit "add axiosClient and topicService for API integration"

--- src/services/topicService.ts
+++
@@ -1,9 +0,0 @@
-import axiosClient from "@/api/axiosClient";
-
-// Lấy danh sách đề tài
-export const getTopics = () =>
-  axiosClient.get("/topics");
-
-//cái này Bằng chỉ làm mẫu 1 api thôi nhé 
-//Cái này phải mở API của BE chạy rồi ghi vào thôi
-
